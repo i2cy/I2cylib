e23a822dd959471d74bdd1de86f9ae8e07f9888f
Icy Cloud <i2cy@outlook.com>
2022-05-18 17:13:07 +0800

    added unlabeled args splitting function in utils.args

--- i2cylib/utils/args/get_args.py
+++ i2cylib/utils/args/get_args.py
@@ -3,19 +3,30 @@
 # OS: ALL
 # Name: Shell Argument(s) Reader
 # Description: This function can get the argument(s) from the Shell
-# Used Librarie(s): sys
+# Used Libraries : sys
+
 
 import sys
 
-def get_args(): # read command shell's argument(s)
+
+def get_args():  # read command shell's argument(s)
+    """
+    read command shell's argument(s)
+
+    :return: dict
+    """
     opts = sys.argv[1:]
     argv = ""
     res = {}
+    unlabeled = 0
     for i in opts:
-        if len(argv) > 0 and "-" != i[0]:
-            res.update({argv:i})
+        if len(argv) > 0 and "-" != i[0]:  # 获取标签
+            res.update({argv: i})
             argv = ""
-        if "-" == i[0]:
+        elif "-" == i[0]:
             argv = i
-            res.update({argv:""})
+            res.update({argv: ""})
+        else:
+            res.update({unlabeled: i})
+            unlabeled += 1
     return res
